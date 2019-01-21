88d1848aedec192806492f285d1b062fd8ec0243
kapitanluffy <kapitanluffy@gmail.com>
2019-01-21 08:46:10 +0000

    Change OpenWeatherMap::get() to return single weather

--- src/AppBundle/Weather/OpenWeatherMap.php
+++ src/AppBundle/Weather/OpenWeatherMap.php
@@ -20,46 +20,11 @@ class OpenWeatherMap implements WeatherInterface
      *
      * @param  string $lat
      * @param  string $lon
-     * @param  int    $atLeast
      *
      * @return array
      */
-    public function get($lat, $lon, $atLeast = 10)
+    public function get($lat, $lon)
     {
-        $weathers = $this->owm->getCitiesInCycle($lat, $lon);
-        $fetched = [];
-        $last = 0;
-
-        while (count($weathers) < $atLeast && $last != count($weathers)) {
-            $last = count($weathers);
-
-            foreach ($weathers as $weather) {
-                // @todo decorate owm with caching
-                if (in_array($weather->city->id, $fetched) == true) continue;
-
-                $extras = $this->owm->getCitiesInCycle($weather->city->lat, $weather->city->lon);
-                $fetched[] = $weather->city->id;
-                $weathers = array_merge($weathers, $extras);
-                $this->unique($weathers);
-            }
-        }
-
-        return array_slice($weathers, 0, $atLeast);
-    }
-
-    protected function unique(&$weathers)
-    {
-        $ids = [];
-
-        foreach ($weathers as $key => $weather) {
-            if (in_array($weather->city->id, $ids) == false) {
-                $ids[] = $weather->city->id;
-                continue;
-            }
-
-            if (in_array($weather->city->id, $ids) == true) {
-                unset($weathers[$key]);
-            }
-        }
+        return $this->owm->getWeather(['lat' => $lat, 'lon' => $lon]);
     }
 }
